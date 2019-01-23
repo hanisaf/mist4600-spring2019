a63b05ff25636915d0355df5b93e86cde4ca8d04
hanisaf <hanisaf@gmail.com>
2019-01-23 10:13:41 -0500

    histogram project

--- HistogramObjects/src/Main.java
+++ HistogramObjects/src/Main.java
@@ -2,17 +2,6 @@
 public class Main {
 
 	public static void main(String[] args) {
-	      Histogram movieReviewScores = new Histogram();
-	      movieReviewScores.setNumberOfBins(6);
-	      
-	      movieReviewScores.setBinValue(0, 1);
-	      movieReviewScores.setBinValue(1, 2);
-	      movieReviewScores.setBinValue(2, 2);
-	      movieReviewScores.setBinValue(3, 4);
-	      movieReviewScores.setBinValue(4, 5);
-	      movieReviewScores.setBinValue(5, 3);
-	      
-	      movieReviewScores.printHistogram();
 
 	}
 
