d37518d50d2a9ec5a8477d83c9f8336b0ec59cdf
hanisaf <hanisaf@gmail.com>
2019-02-05 20:23:10 -0500

    resturant project

---
+++ RestaurantRating/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		Restaurant favLunchPlace = new Restaurant();
+		Restaurant favDinnerPlace = new Restaurant();
+
+		favLunchPlace.setName("Central Deli");
+		favLunchPlace.setRating(4);
+
+		favDinnerPlace.setName("Friends Cafe");
+		favDinnerPlace.setRating(5);
+
+		System.out.println("My favorite restaurants: ");
+		favLunchPlace.print();
+		favDinnerPlace.print();
+	}
+}
