e8a94133d4c73c6fb0d41b21f3b551eeec492763
hanisaf <hanisaf@gmail.com>
2019-02-20 12:06:28 -0500

    Update Example2.java

--- LoopExample/src/Example2.java
+++ LoopExample/src/Example2.java
@@ -2,17 +2,17 @@
 public class Example2 {
 
 	public static void main(String[] args) {
-		// counting from 1 to 10
+		// counting from 1 to 5
 		
 
 
 //		int i=1;
-//		while(i <= 10) {
+//		while(i <= 5) {
 //		    System.out.println(i);
 //		    i=i+1;
 //		} 
 		
-//		for(int i=1; i<=10; i=i+1) {
+//		for(int i=1; i<=5; i=i+1) {
 //		    System.out.println(i);
 //		}
 
