7898890a1c77e69becb6b3434fe6eabfe3be0b5a
Hani Safadi <hanisaf@uga.edu>
2019-02-22 09:37:52 -0500

    loop example 1

---
+++ LoopExample_1/src/Example2.java
@@ -0,0 +1,21 @@
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// counting from 1 to 10
+		
+
+
+		int i=1;
+		while(i <= 5) {
+		    System.out.println(i);
+		    i=i+1;
+		} 
+		
+//		for(int i=1; i<=5; i=i+1) {
+//		    System.out.println(i);
+//		}
+
+	}
+
+}
