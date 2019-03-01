6bbc77929ab54beea29305d94a939c1cfa5ecf8e
hanisaf <hanisaf@gmail.com>
2019-03-01 09:15:39 -0500

    array example 1

---
+++ ArrayExample_1/src/Example4.java
@@ -0,0 +1,14 @@
+
+public class Example4 {
+
+	public static void main(String[] args) {
+		double[] prices = {33.4, 55.0, 109.99, 234.0, 66.6};
+
+		//calculate the total amount in the shopping cart i.e., sum prices
+		
+		
+		//There is a discount, the store will forgo the cents in each price, e.g., 33.4 will be 33
+		//what is the new total price
+	}
+ 
+}
