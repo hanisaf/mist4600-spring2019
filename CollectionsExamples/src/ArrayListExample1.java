c92e78fc18cf362d24f79367c51b1d40c3bd07ca
hanisaf <hanisaf@gmail.com>
2019-03-29 10:18:14 -0400

    collection examples

---
+++ CollectionsExamples/src/ArrayListExample1.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class ArrayListExample1 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> itemCounts; //declaration
+		
+		itemCounts = new ArrayList<Integer>(); //initialization, notice size is not needed
+
+		itemCounts.add(122); //accessing, "write"
+		itemCounts.add(119);
+		itemCounts.add(117);
+		
+		System.out.println(itemCounts.get(1)); //accessing, "read"
+		
+		itemCounts.set(1, -119); //accessing, "modify"
+		
+		System.out.println(itemCounts.get(1));
+		
+		System.out.println(itemCounts.size());
+		
+		itemCounts.remove(1);
+		
+		int item = itemCounts.get(1);
+		System.out.println(item);
+		System.out.println(itemCounts.size());
+		
+	}
+
+}
